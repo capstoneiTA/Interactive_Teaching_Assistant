cbf048a72e2e6805bc16a9359af8ca5ef947688c
Jared Gross <tuk12216@temple.edu>
2020-09-23 17:57:50 -0400

    setup basic communication between containers

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
 //Import express for db requests
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+
+//Prevent cors issues from inter-container communication
+app.use(cors());
 
 //Set up port
 const port = 5000;
@@ -10,6 +15,10 @@ const db = require("./models");
 
 //Sync database tables
 db.sequelize.sync({force:true});
+
+app.get('/word', (req,res)=>{
+    res.send('Response from the database!');
+});
 
 //Start listening for connections
 app.listen(port, () =>{
--- ui/src/helloWorld.js
+++ ui/src/helloWorld.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 
-function HelloWorld(props){
-    return(
-        <h1 className="helloWorld">
-            Hello World!!
-        </h1>
-    )
+const apiUrl = `http://localhost:8080`;
+
+class HelloWorld extends Component {
+    state = {
+        word: ""
+    };
+
+    async loadWord() {
+        const res = await axios.get(apiUrl + '/word');
+        console.log(res);
+        this.setState({
+            word: res.data
+        });
+    }
+
+    //Runs after the component is mounted
+    componentDidMount() {
+        this.loadWord();
+    }
+
+    render(){
+        return(
+            <h1 className="helloWorld">
+                {this.state.word}
+            </h1>
+        )
+    }
+
+
 }
 
 export default HelloWorld;
