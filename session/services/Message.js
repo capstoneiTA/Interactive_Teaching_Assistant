171015549c70c2f5f891307792892dbff9a82a81
Jack-Tilley <tilley.e.jack@gmail.com>
2020-09-28 10:17:11 -0400

    added skeleton for Message class

---
+++ session/services/Message.js
@@ -0,0 +1,36 @@
+class Message {
+    constructor(id, content, replyTo, user){
+        this.id = id;
+        this.content = content;
+        this.replyTo = replyTo;
+        this.user = user
+        this.createTime = new Date();
+    }
+    getId(){
+
+    }
+    setId(id){
+
+    }
+    getContent(){
+
+    }
+    getReplyTo(){
+
+    }
+    setReplyTo(replyTo){
+
+    }
+    getUser(){
+
+    }
+    setUser(user){
+
+    }
+    getCreateTime(){
+
+    }
+    setCreateTime(createTime){
+
+    }
+}
