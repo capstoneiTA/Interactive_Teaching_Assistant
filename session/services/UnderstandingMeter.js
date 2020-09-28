fb0e1ef784cbabb02d5bc3ce193270ba42b67baf
tuanh <tuan.huynh@temple.edu>
2020-09-28 10:51:59 -0400

    Add constructor for UM

---
+++ session/services/UnderstandingMeter.js
@@ -0,0 +1,35 @@
+class UnderstandingMeter{
+
+    /*
+    * Creating an understanding meter object
+    * @constructor
+    * @param {int} score - the score from student
+    * @param {string} user - name of the user
+    * @param {object} history - record changes {K = time: V = {score:int}}
+    * */
+    constructor(score, user, history) {
+        this.score = score;
+        this.user = user;
+        this.history = history;
+    }
+
+    getScore() {
+
+    }
+
+    setScore() {
+
+    }
+
+    getUser() {
+
+    }
+
+    recordChange() {
+
+    }
+
+    getTime() {
+
+    }
+}
