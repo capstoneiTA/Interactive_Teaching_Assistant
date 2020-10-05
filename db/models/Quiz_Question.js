02cb56674b0a89ce4385263ca911f8ebd4534979
tanGarciaC <60166249+tanGarciaC@users.noreply.github.com>
2020-10-04 20:59:15 -0400

    db tables for quiz

---
+++ db/models/Quiz_Question.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes)=> {
+    const Quiz_Question = sequelize.define('Quiz_Question', {
+        //model for Quiz_question db
+        Quiz_Question_ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            unique: true
+        },
+        Quiz_ID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: false,
+            references: {
+            model: 'quiz',
+            key: 'Quiz_ID'
+            }
+        },
+       Prompt: {
+            type: DataTypes.STRING(100);
+            allowNull: false,
+            unique: true
+       }
+
+
+    });
+    return Quiz_Question;
+};
---
+++ db/models/Quiz_Question_Response.js
@@ -0,0 +1,64 @@
+module.exports= (sequelize, DataTypes)=> {
+    const Quiz_Question_Response = sequelize.define('Quiz_Question_Response', {
+    //model for quiz question response db attributes
+    Quiz_Response_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+    },
+    User_ID:{
+        type: DataTypes.STRING(100),
+        allowNUll: false,
+        unique: false,
+        references: {
+            model: 'User',
+            key: 'User_ID'
+        }
+    },
+    Quiz_Question_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: false,
+        references:{
+        model: 'Quiz_Question',
+        key: 'Quiz_Question_ID'
+        }
+    },
+    FITB_Option_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: false,
+        references:{
+            model: 'Fill_In_The_Blank_Option'
+            key: 'FITB_Option_ID'
+        }
+    },
+
+    MC_Option_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: false,
+        references:{
+            model: 'Multiple_Choice_Option'
+            key: 'MC_Option_ID'
+        }
+
+    },
+    Session_ID: {
+        type: DataTypes.String(100),
+        allowNull: false,
+        unique: false,
+        references: {
+            model: 'Session',
+            key: 'Session_ID'
+        }
+
+
+    }
+
+
+    });
+    return Quiz_Question_Response;
+};
