574ee13cca27f404cb6a1190e930c6f44cd9a06c
Jack-Tilley <tilley.e.jack@gmail.com>
2020-10-04 21:56:50 -0400

    added quiz options models for fillintheblank and multiplechoice

---
+++ db/models/multiple_choice_option.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes)=> {
+    const Poll_Option = sequelize.define('Multiple_Choice_Option', {
+        // Model attributes are defined here
+        Multiple_Choice_Option_ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            unique: true
+        },
+        Quiz_Question_ID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: false,
+            references: {
+                model: 'Quiz_Question',
+                key: 'Quiz_Question_ID'
+            }
+        },
+        Option_Text: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: false
+        },
+        isCorrect: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            unique: false
+        }
+    });
+
+    return Multiple_Choice_Option;
+};
