91ff294e9c84f14d94013fafbea87d13ab568ce3
tuanh <tuan.huynh@temple.edu>
2020-10-13 14:38:44 -0400

    IN progress of setting up UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,9 @@
 import React, {Component} from 'react';
 import '../css/uMeter.css';
 
-const apiUrl = `http://localhost:9000`;
+const apiUrl = `http://localhost:8080`;
 
 class App extends Component {
-
-    render() {
-        return (
-            <div class="slidecontainer">
-                <input type="range" min="1" max="5" value="3" class="slider" id="myRange"/>
-            </div>
-        )
-    }
-
 
 }
 
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import '../css/uMeter.css'
+
+class slider extends Component {
+    render() {
+        return <input type="range" min="1" max="5" value="3" className="slider" id="myRange"/>;
+    }
+}
 
 ReactDOM.render(
     <React.StrictMode>
+        <slider />
+    </React.StrictMode>;
 
-    </React.StrictMode>
+    document.getElementById('root')
 );
