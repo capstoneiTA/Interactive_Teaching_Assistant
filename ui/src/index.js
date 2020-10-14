f0a0d8bf3e0e66b9d8a4fed1ef7bacc3d661d5c0
tuanh <tuan.huynh@temple.edu>
2020-10-13 21:37:04 -0400

    Reset code for ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,16 +2,5 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../css/uMeter.css'
 
-class slider extends Component {
-    render() {
-        return <input type="range" min="1" max="5" value="3" className="slider" id="myRange"/>;
-    }
-}
 
-ReactDOM.render(
-    <React.StrictMode>
-        <slider />
-    </React.StrictMode>;
 
-    document.getElementById('root')
-);
--- ui/src/uMeter_UI.js
+++
@@ -1,8 +0,0 @@
-const apiUrl = `http://localhost:8080`;
-
-function slidecontainer() {
-    return <input type="range" min="1" max="100" value="50" className="slider" id="myRange"/>
-}
-
-
-
