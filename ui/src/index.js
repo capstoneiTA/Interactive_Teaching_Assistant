1153ef9f9497d446e85b39c9fcc6accdeb650bb9
tuanh <tuan.huynh@temple.edu>
2020-10-13 22:53:47 -0400

    Install required dependency for styling

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,2 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import uMeter from "./uMeter_UI";
+
+ReactDOM.render(
+    <uMeter />,
+    document.getElementById('root')
+);
