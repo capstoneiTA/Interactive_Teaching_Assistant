94d9d988dd3d634b3489fea0f9d1cbfa9a7f785a
tuanh <tuan.huynh@temple.edu>
2020-10-29 19:35:27 -0400

    Add get quiz code in db

--- db/endpoints/quiz-api.js
+++ db/endpoints/quiz-api.js
@@ -13,134 +13,101 @@ module.exports = function(app, db) {
             User_ID: userId,
             Quiz_Name: quiz.quizName,
             Quiz_Type: quizType
-        }).then(function(Quiz){
+        }).then(function (Quiz) {
             response.quizCreation = true;
-            quiz.quizQuestions.forEach(function(question){
+            quiz.quizQuestions.forEach(function (question) {
                 db.QuizQuestion.create({
                     Quiz_ID: Quiz.Quiz_ID,
                     Prompt: question.prompt
-                }).then(function(Question){
-                    if(quizType === 'Multiple Choice'){
-                        question.options.forEach(function(option, index){
+                }).then(function (Question) {
+                    if (quizType === 'Multiple Choice') {
+                        question.options.forEach(function (option, index) {
                             db.Multiple_Choice_Option.create({
                                 Quiz_Question_ID: Question.Quiz_Question_ID,
                                 Option_Text: option,
                                 isCorrect: question.corrects[index]
-                            }).then(function(){
+                            }).then(function () {
                                 //Option add success!
-                            }).catch(function(error){
-                                console.log('Error adding option ' + (index+1));
+                            }).catch(function (error) {
+                                console.log('Error adding option ' + (index + 1));
                             })
                         })
                     }
-                }).catch(function(error){
+                }).catch(function (error) {
                     response.questionsCreate = false;
                     res.send(response);
                 });
-                questionsAdded ++;
-                if(questionsAdded === quiz.quizQuestions.length){
+                questionsAdded++;
+                if (questionsAdded === quiz.quizQuestions.length) {
                     response.questionsCreate = true;
                     res.send(response);
                 }
             });
 
-        }).catch(function(error){
+        }).catch(function (error) {
             response.quizCreation = false;
             res.send(response);
         })
 
     });
 
-    app.get("/quiz/retrieve", function(req, res) {
+    app.get("/quiz/retrieve", function (req, res) {
         //Get session creation data from post request
         let userId = req.query.userId;
         let response = {};
-        let quizzes = [];
 
         let foundQuizzes = getQuizzes(userId, res, response);
-        foundQuizzes.then((Quizzes)=>{
-
-        });
         res.send(true);
-
-
     });
 
-    async function getQuizzes(userId, res, response){
-        let foundQuizzes = await db.Quiz.findAll({
+    async function getQuizzes(userId, response) {
+        let quiz = {};
+        let question = {};
+        let option_object = {};
+
+        let quizForUser = await db.Quiz.findAll({
             where: {
                 User_ID: userId
             }
-        }).then(async function(Quizzes){
-            let foundQuizzes = [];
-            if(Quizzes.length > 0){
-                response.anyQuizzes = true;
-                for(let Quiz of Quizzes){
-                    foundQuizzes.push(Quiz);
-                }
-            }else{
-                response.anyQuizzes = false;
-                res.send(response);
-            }
-            return foundQuizzes;
-        }).catch(function(error){
-            res.send(error);
         });
 
-        return foundQuizzes;
-    }
+        if (quizForUser.length > 0) {
+            response.anyQuizzes = true;
 
-    async function getQuizQuestions(Quizzes){
-        let quizzes = []
-        for(let Quiz of Quizzes){
-            let quiz = {};
-            quiz.quizName = Quiz.Quiz_Name;
-            quiz.quizQuestions = [];
+            let quizQuestions = await quizForUser.map((Quiz) => {
+                //Get info for quiz object
+                quiz.quizName = Quiz.Quiz_Name;
+                quiz.quizQuestionsList = [];
+                //main code
+                db.QuizQuestion.findAll({
+                    where: {
+                        Quiz_ID: Quiz.Quiz_ID
+                    }
+                })
+            });
+
+            let quizOptions = await quizQuestions.map((Question) => {
+                question.prompt = Question.Prompt;
+                question.options =[];
+
+                db.Multiple_Choice_Option.findAll({
+                    where: {
+                        Quiz_Question_ID: Question.Quiz_Question_ID
+                    }
+                })
+            });
+
+            let optionDetails = await quizOptions.map((Option) => {
+                option_object.option = option.Option_Text;
+                option_object.isCorrect = option.isCorrect;
+                question.options.push(option_object);
+            });
+
+            let createRes = await optionDetails;
+            quiz.quizQuestionsList.push(question);
+
+            console.log(quiz);
+            return quiz;
         }
     }
-
-
-
-    // response.anyQuizzes = true;
-    //
-    // await Promise.all(Quizzes.map((Quiz)=>{
-    //     let quiz = {};
-    //     quiz.quizName = Quiz.Quiz_Name;
-    //     quiz.quizQuestions = [];
-    //     db.QuizQuestion.findAll({
-    //         where: {
-    //             Quiz_ID: Quiz.Quiz_ID
-    //         }
-    //     }).then( async function(Questions){
-    //
-    //         await Promise.all(Questions.map((question)=>{
-    //             let question_object = {};
-    //             question_object.prompt = question.Prompt;
-    //             question_object.options = [];
-    //             db.Multiple_Choice_Option.findAll({
-    //                 where:{
-    //                     Quiz_Question_ID: question.Quiz_Question_ID
-    //                 }
-    //             }).then(async function(Options){
-    //
-    //                 await Promise.all(Options.map((option)=>{
-    //                     let option_object = {};
-    //                     option_object.option = option.Option_Text;
-    //                     option_object.isCorrect = option.isCorrect;
-    //                     question_object.options.push(option_object);
-    //                 }));
-    //             }).catch(function(error){
-    //                 console.log(error.message);
-    //                 // res.send(error); //option retrieval error
-    //             });
-    //
-    //             quiz.quizQuestions.push(question_object);
-    //         }));
-    //
-    //     }).catch(function(error){
-    //         res.send(error); //Question Retrieval Error
-    //     });
-    //     quizzes.push(quiz);
-    // }));
-
 };
