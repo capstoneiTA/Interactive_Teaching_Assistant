3c39a1dcae29d229080ae9c6eb29e8be94d9383a
tuanh <tuan.huynh@temple.edu>
2020-11-09 09:34:26 -0500

    Small changes

--- ui/src/components/SessionComps/TeacherClassSessionMenu.js
+++ ui/src/components/SessionComps/TeacherClassSessionMenu.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
