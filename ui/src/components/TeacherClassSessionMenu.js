433d4f2204a54073c1f0b3d8891710aa963866c7
tuanh <tuan.huynh@temple.edu>
2020-11-09 09:59:33 -0500

    Update bug fix

--- ui/src/components/TeacherClassSessionMenu.js
+++ ui/src/components/TeacherClassSessionMenu.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -15,11 +13,13 @@ function TabPanel(props) {
             hidden={value !== index}
             id={`vertical-tabpanel-${index}`}
             aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            <div style={{ display: value === 0? 'block': 'none'}}>
+            {...other}>
+            <div>the value is: {value}</div>
+            <div>The index is: {index}</div>
+            <div style={{ display: value === index ? 'block': 'none'}}>
                 {children}
             </div>
+
         </div>
     );
 }
