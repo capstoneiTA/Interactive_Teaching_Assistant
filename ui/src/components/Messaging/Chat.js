df0b352add95c5091b88e1e0dbf6c31f073e717d
Jack-Tilley <tilley.e.jack@gmail.com>
2020-11-12 23:47:53 -0500

    changed messaging position

--- ui/src/components/Messaging/Chat.js
+++ ui/src/components/Messaging/Chat.js
@@ -1,90 +1,89 @@
-import React, {useContext, useEffect, useState} from 'react';
-import socketIOClient from 'socket.io-client';
+import React, { useContext, useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
 import axios from "axios";
 
-import {QuizContext} from "../ActivityCreation/QuizContext";
-import {ChatContext} from "./ChatContext";
-import ChatBox from './ChatBox';
+import { QuizContext } from "../ActivityCreation/QuizContext";
+import { ChatContext } from "./ChatContext";
+import ChatBox from "./ChatBox";
 
-
-let apiUrl = '';
-let ENDPOINT = '';
-if(process.env.REACT_APP_DEPLOY === "False"){
-    apiUrl = `http://localhost:8080`;
-    ENDPOINT = `http://localhost:7000/`;
-
-}else{
-    apiUrl = `${process.env.REACT_APP_EC2HOST}:8080`;
-    ENDPOINT = `${process.env.REACT_APP_EC2HOST}:7000/`;
+let apiUrl = "";
+let ENDPOINT = "";
+if (process.env.REACT_APP_DEPLOY === "False") {
+  apiUrl = `http://localhost:8080`;
+  ENDPOINT = `http://localhost:7000/`;
+} else {
+  apiUrl = `${process.env.REACT_APP_EC2HOST}:8080`;
+  ENDPOINT = `${process.env.REACT_APP_EC2HOST}:7000/`;
 }
 
+const Chat = ({ user, sessionName, sessionId }) => {
+  const [value, setValue] = useState("");
+  const { messages, setMessages } = useContext(ChatContext);
+  let socket = socketIOClient(ENDPOINT + sessionName);
+  let sockid = "";
 
-const Chat = ({user, sessionName, sessionId}) => {
-    const [value, setValue] = useState('');
-    const {messages, setMessages} = useContext(ChatContext);
-    let socket = socketIOClient(ENDPOINT + sessionName);
-    let sockid = '';
+  useEffect(() => {
+    axios
+      .post(apiUrl + "/chat/join", { sessionName: sessionName })
+      .then(function (res) {
+        if (res.data.chat_created === true) {
+          socket.on("connect", function () {
+            sockid = socket.id;
+          });
+          listen();
+        } else {
+          console.log("chat listener creation error");
+        }
+      });
+  }, []);
 
-
-    useEffect(()=>{
-        axios.post(apiUrl + '/chat/join', {sessionName: sessionName}).then(function (res) {
-            if(res.data.chat_created === true){
-                socket.on('connect', function(){
-                    sockid = socket.id;
-                });
-                listen();
-            }else{
-                console.log('chat listener creation error');
-            }
-        });
-    }, []);
-
-
-
-    useEffect(()=>{
-        updateMessages=(data)=>{
-            setMessages(data);
-        };
-    }, [messages]);
-
-    const listen =()=>{
-        socket.on('chat message from server', function(data){
-            // console.log('DATA: ', data)
-            // console.log('Incoming message for : ' + sockid +  ' ' + data[0]+ ' ' + data[1]);
-            //Update messages state
-            updateMessages(data);
-        });
+  useEffect(() => {
+    updateMessages = (data) => {
+      setMessages(data);
     };
+  }, [messages]);
 
-    let updateMessages=(data)=>{
-        console.log("The first definition");
-        let newMessages = [...messages];
-        newMessages.push(data);
-        setMessages(newMessages);
-        // console.log(newMessages);
-    };
+  const listen = () => {
+    socket.on("chat message from server", function (data) {
+      // console.log('DATA: ', data)
+      // console.log('Incoming message for : ' + sockid +  ' ' + data[0]+ ' ' + data[1]);
+      //Update messages state
+      updateMessages(data);
+    });
+  };
 
+  let updateMessages = (data) => {
+    console.log("The first definition");
+    let newMessages = [...messages];
+    newMessages.push(data);
+    setMessages(newMessages);
+    // console.log(newMessages);
+  };
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setValue(e.target.value)
-      };
-    
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // console.log('submitting: ', value);
-      socket.emit('chat message from client', value, user);
-      setValue('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log('submitting: ', value);
+    socket.emit("chat message from client", value, user);
+    setValue("");
 
-      //Clear text box here
-    };
+    //Clear text box here
+  };
 
+  return (
+    <>
+      <ChatBox
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        value={value}
+        messages={messages}
+        user={user}
+      />
 
-    return (
-        <>
-        <ChatBox handleChange={handleChange} handleSubmit={handleSubmit} value={value} messages={messages}/>
-
-        {/* <h1>CHAT</h1>
+      {/* <h1>CHAT</h1>
         <form onSubmit={e => handleSubmit(e)}>
         <label>
           Name:
@@ -97,8 +96,8 @@ const Chat = ({user, sessionName, sessionId}) => {
           <p>{msg}</p>
         ))}
       </div> */}
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default Chat
+export default Chat;
--- ui/src/components/Messaging/ChatBox.js
+++ ui/src/components/Messaging/ChatBox.js
@@ -1,106 +1,134 @@
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import FolderIcon from "@material-ui/icons/Folder";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Button from '@material-ui/core/Button';
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import React from 'react';
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-      position: 'relative',
-      overflow: 'auto',
-      maxHeight: 300,
-      margin: 3,
-      border: '2px solid blue',
-      borderRadius: 6
-    },
-    listSection: {
-      backgroundColor: 'inherit',
-    },
-    ul: {
-      backgroundColor: 'inherit',
-      padding: 0,
-    },
-    form: {
-        margin: 5,
-        padding: 5,
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing.unit,
-      },
-    h1: {
-        textAlign: 'center' 
-    }
-  }));
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    position: "relative",
+    overflow: "auto",
+    maxHeight: 300,
+    margin: 3,
+    border: "2px solid blue",
+    borderRadius: 6,
+  },
+  listSection: {
+    backgroundColor: "inherit",
+  },
+  ul: {
+    backgroundColor: "inherit",
+    padding: 0,
+  },
+  form: {
+    margin: 5,
+    padding: 5,
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  h1: {
+    textAlign: "center",
+  },
+}));
 
-const ChatBox = ({handleSubmit, handleChange, value, messages}) => {
-    const classes = useStyles();
-      return (
-        <>
-        <div className={classes.root}>
-            <h1>Chat</h1>
-            <List dense={true}>
-            {messages.map((msg) => (
-                <ListItem key={msg}>
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={msg[1]}
-                  secondary={msg[0]}
-                />
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete">
-                    <MoreVertIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            ))}
-            </List>
-            <form className={classes.form} onSubmit={e => handleSubmit(e)}>
-                <FormControl margin="normal" fullWidth>
-                    <InputLabel htmlFor="message"></InputLabel>
-                    <Input id="message" placeholder="Send something nice!" name="message" value={value} onChange={handleChange} autoFocus autoComplete="off"/>
-                </FormControl>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    onClick={e => handleSubmit(e)}
+const ChatBox = ({ handleSubmit, handleChange, value, messages, user }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.root}>
+        <h1>Chat</h1>
+        <List dense={true}>
+          {messages.map((msg) => {
+            if (msg[1] === user.firstName + " " + user.lastName) {
+              return (
+                <ListItem
+                  key={msg}
+                  style={{ textAlign: "right", color: "blue" }}
                 >
-                    Send Message
-                </Button>
-            </form>
-            </div>
-          </>
-      )
-}
-export default ChatBox
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={msg[1]} secondary={msg[0]} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <MoreVertIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            } else {
+              return (
+                <ListItem key={msg}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={msg[1]} secondary={msg[0]} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <MoreVertIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            }
+          })}
+        </List>
+        <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="message"></InputLabel>
+            <Input
+              id="message"
+              placeholder="Send something nice!"
+              name="message"
+              value={value}
+              onChange={handleChange}
+              autoFocus
+              autoComplete="off"
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Send Message
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default ChatBox;
